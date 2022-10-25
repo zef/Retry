7fc5e484fd82e38cfc802c4527f29b5fab9df902
Zef Houssney <zef@zef.studio>
2022-10-25 11:54:34 -0600

    Fix access control problems.

--- Sources/Retry/Retry.swift
+++ Sources/Retry/Retry.swift
@@ -5,30 +5,29 @@
 //  Created by Zef Houssney on 1/27/16.
 //
 
-
 import Foundation
 
-class Retry {
-    typealias ErrorParams = [String: Any]
+public class Retry {
+    public typealias ErrorParams = [String: Any]
 
-    struct Callbacks {
-        typealias SuccessCallback = (_ description: String, _ attempt: Int) -> Void
-        typealias ErrorCallback = (_ description: String, _ attempt: Int, _ errorParams: ErrorParams?) -> Void
+    public struct Callbacks {
+        public typealias SuccessCallback = (_ description: String, _ attempt: Int) -> Void
+        public typealias ErrorCallback = (_ description: String, _ attempt: Int, _ errorParams: ErrorParams?) -> Void
 
-        var onSuccess: SuccessCallback?
-        var onSuccessAfterRetry: SuccessCallback?
-        var onRetryAttempt: ErrorCallback?
-        var onFinalFailure: ErrorCallback?
+        public var onSuccess: SuccessCallback?
+        public var onSuccessAfterRetry: SuccessCallback?
+        public var onRetryAttempt: ErrorCallback?
+        public var onFinalFailure: ErrorCallback?
     }
 
-    static var defaultCallbacks = Callbacks()
+    public static var defaultCallbacks = Callbacks()
 
     let callbacks: Callbacks
     let description: String
     var attemptsAllowed: Int = 1
-    fileprivate var attemptBlock: (Retry) -> Void
+    var attemptBlock: (Retry) -> Void
 
-    fileprivate(set) var currentAttempt: Int = 1
+    public private(set) var currentAttempt: Int = 1
 
     init(_ description: String, retries: Int = 1, callbacks: Callbacks = Retry.defaultCallbacks, block: @escaping (Retry) -> Void) {
         self.description = description
@@ -37,19 +36,19 @@ class Retry {
         self.callbacks = callbacks
     }
 
-    static func attempt(_ description: String, retries: Int = 1, callbacks: Callbacks = Retry.defaultCallbacks, block: @escaping (Retry) -> Void) {
+    public static func attempt(_ description: String, retries: Int = 1, callbacks: Callbacks = Retry.defaultCallbacks, block: @escaping (Retry) -> Void) {
         let attempt = Retry(description, retries: retries, callbacks: callbacks, block: block)
         attempt.attemptBlock(attempt)
     }
 
-    func success() {
+    public func success() {
         callbacks.onSuccess?(description, currentAttempt)
         if currentAttempt > 1 {
             callbacks.onSuccessAfterRetry?(description, currentAttempt)
         }
     }
 
-    func failure(_ errorParams: ErrorParams? = nil, block failureBlock: () -> Void ) {
+    public func failure(_ errorParams: ErrorParams? = nil, block failureBlock: () -> Void ) {
         if currentAttempt < attemptsAllowed {
             callbacks.onRetryAttempt?(description, currentAttempt, errorParams)
             currentAttempt += 1
